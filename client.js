0ad1a58774f41f380610b20316ea080311b41c25
Saurabh Thakur <mail@saurabhthakur.dev>
2021-01-03 23:46:12 +0530

    ADD: client to read/write from the server

---
+++ client.js
@@ -0,0 +1,31 @@
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const packageDef = protoLoader.loadSync('todo.proto', {});
+
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const client = new todoPackage.Todo('localhost:4000', grpc.credentials.createInsecure());
+
+client.createTodo(
+    {
+        id: -1,
+        text: 'Do this',
+    },
+    (error, response) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('Received from server', response);
+
+            client.readTodos({}, (error, response) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log('Received from server', response);
+                }
+            });
+
+        }
+    }
+);
