91eddc9a4e328b03283989dfa13d3c5d445df851
Saurabh Thakur <mail@saurabhthakur.dev>
2021-01-03 23:46:57 +0530

    ADD: server side code for stream

--- server.js
+++ server.js
@@ -18,11 +18,18 @@ function createTodoNative(call, callback) {
 function readTodosNative(call, callback) {
     callback(null, { items: todos });
 }
+function readTodoStreamNative(call, callback) {
+    todos.forEach((todo) => {
+        call.write(todo);
+    });
+    call.end();
+}
 
 const server = new grpc.Server();
 server.addService(todoPackage.Todo.service, {
     createTodo: createTodoNative,
     readTodos: readTodosNative,
+    readTodoStream: readTodoStreamNative,
 });
 
 server.bind('0.0.0.0:4000', grpc.ServerCredentials.createInsecure());
