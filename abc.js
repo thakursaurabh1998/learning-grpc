7eb9642c3b24b7e1a057c7a0aeaf65604c702f2f
Saurabh Thakur <mail@saurabhthakur.dev>
2021-08-07 01:34:27 +0530

    ADD: second line log

--- abc.js
+++ abc.js
@@ -3,5 +3,8 @@ console.log('Hello World');
 console.log('fixup1');
 console.log('fixup2');
 
+<<<<<<< HEAD
 console.log('ok');
+=======
+>>>>>>> f98d74a... ADD: second line log
 console.log('This is second line log');
